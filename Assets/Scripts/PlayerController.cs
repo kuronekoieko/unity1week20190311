1b64d1f4d6747192e01f8a96f05d5da2090ee82a
ieko <mendo931411.mou@gmail.com>
2019-03-12 00:52:25 +0900

    プレイヤーコントローラー作成

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    float walkSpeed = 10.0f;
+    int key_x = 0;
+    int key_y = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(key_x, key_y) * walkSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        key_x = 0;
+        key_y = 0;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            key_y = 1;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            key_y = -1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            key_x = 1;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            key_x = -1;
+        }
+
+
+    }
+}
