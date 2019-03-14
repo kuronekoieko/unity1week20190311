0f3487f02ba950691cc310ed623fd57a9c83b97a
ieko <mendo931411.mou@gmail.com>
2019-03-14 22:59:32 +0900

    Uiとか数値を調整

--- Assets/Scripts/Common/Variable.cs
+++ Assets/Scripts/Common/Variable.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class Variable
 {
-    public static float walkSpeed = 15.0f;
+    public static float walkSpeed = 10.0f;
 
     public static int catCount;
 
     public static bool isTimeUp;
 
-    public static float limitTimeSec = 30;
+    public static float limitTimeSec = 60;
 
-    public static float cam_range_x = 3.0f;
+    public static float cam_range_x = 1.0f;
     public static float cam_range_y = 0.5f;
 }
